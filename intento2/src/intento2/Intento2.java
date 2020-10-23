84408bb63c5db7e8d998b1e64b445a95c49701b4
walter reyes <walter.reyes@cambridgehn.com>
2020-10-22 20:22:14 -0600

    comenta2

--- intento2/src/intento2/Intento2.java
+++ intento2/src/intento2/Intento2.java
@@ -7,11 +7,9 @@ public class Intento2 {
   
     public static void main(String[] args) {
 
-        System.out.println("h");
-        System.out.println("h");
-        System.out.println("h");
-        System.out.println("h");
-        System.out.println("h");
+        System.out.println("hola");
+        System.out.println("estoy");
+        System.out.println("Aquí");
     }
     
 }
