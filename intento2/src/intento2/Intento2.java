ad3630926fa51f7dd58ee6ecd8adcf0f09b7caa6
walter reyes <walter.reyes@cambridgehn.com>
2020-10-22 20:19:57 -0600

    mensaje x

---
+++ intento2/src/intento2/Intento2.java
@@ -0,0 +1,17 @@
+
+package intento2;
+
+
+public class Intento2 {
+
+  
+    public static void main(String[] args) {
+
+        System.out.println("h");
+        System.out.println("h");
+        System.out.println("h");
+        System.out.println("h");
+        System.out.println("h");
+    }
+    
+}
